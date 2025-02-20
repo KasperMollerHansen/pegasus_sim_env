bb48b44c1c9d5b6d233dc91459c23ceb46b17f86
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-02-20 13:31:56 +0100

    update

--- pegasus_simulation/pegasus_sim.py
+++ pegasus_simulation/pegasus_sim.py
@@ -98,7 +98,7 @@ class PegasusApp:
         )
 
         body_frame = XFormPrim(
-            prim_path=prim_path + "/body_frame",
+            prim_path=prim_path + "/body",
             position=position,
         )
 
@@ -238,7 +238,7 @@ class PegasusApp:
         )
 
     def _publish_tf(self, frames):
-        topic_name = self.topic_prefix + "/tf"
+        topic_name = "/tf"
         og.Controller.edit(
             {"graph_path": "/Graphs/ROS_TF", "evaluator_name": "execution"},
             {
