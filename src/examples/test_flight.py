8c81b8158f5c70fe2282511015d862ce9ce0fdee
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-02-21 13:17:41 +0100

    update

--- src/examples/test_flight.py
+++ src/examples/test_flight.py
@@ -9,7 +9,6 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
-import numpy as np
 
 class TestFlight(Node):
     """Node for controlling a vehicle in offboard mode."""
@@ -25,19 +24,8 @@ class TestFlight(Node):
             depth=1
         )
 
-        # Create publishers
-        self.offboard_control_mode_publisher = self.create_publisher(
-            OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
         self.trajectory_setpoint_publisher = self.create_publisher(
-            TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
-        self.vehicle_command_publisher = self.create_publisher(
-            VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
-
-        # Create subscribers
-        self.vehicle_odometry_subscriber = self.create_subscription(VehicleOdometry, '/fmu/out/vehicle_odometry', self.vehicle_odometry_callback, qos_profile)
-        
-        self.vehicle_status_subscriber = self.create_subscription(
-            VehicleStatus, '/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile)
+            TrajectorySetpoint, '/target_setpoint', qos_profile)
         
         # tf topic subscriber
         self.tf_buffer = Buffer()
@@ -53,111 +41,23 @@ class TestFlight(Node):
         # Create a timer to publish control commands
         self.timer = self.create_timer(0.1, self.timer_callback)
 
-    def vehicle_odometry_callback(self, vehicle_odometry):
-        """Callback function for vehicle_odometry topic subscriber."""
-        self.vehicle_odometry = vehicle_odometry
-
-    def vehicle_status_callback(self, vehicle_status):
-        """Callback function for vehicle_status topic subscriber."""
-        self.vehicle_status = vehicle_status
-
-    def arm(self):
-        """Send an arm command to the vehicle."""
-        self.publish_vehicle_command(
-            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
-        self.get_logger().info('Arm command sent')
-
-    def disarm(self):
-        """Send a disarm command to the vehicle."""
-        self.publish_vehicle_command(
-            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=0.0)
-        self.get_logger().info('Disarm command sent')
-
-    def engage_offboard_mode(self):
-        """Switch to offboard mode."""
-        self.publish_vehicle_command(
-            VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
-        self.get_logger().info("Switching to offboard mode")
-
-    def land(self):
-        """Switch to land mode."""
-        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
-        self.get_logger().info("Switching to land mode")
-
-    def publish_offboard_control_heartbeat_signal(self):
-        """Publish the offboard control mode."""
-        msg = OffboardControlMode()
-        msg.position = True
-        msg.velocity = False
-        msg.acceleration = False
-        msg.attitude = False
-        msg.body_rate = False
-        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
-        self.offboard_control_mode_publisher.publish(msg)
-
     def publish_position_setpoint(self, position: list, yaw: float) -> None:
         """Publish the trajectory setpoint."""
         msg = TrajectorySetpoint()
-        msg.position = self.trans_pos(position)
-        msg.yaw = self.trans_yaw(yaw)
+        msg.position = position
+        msg.yaw = yaw
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.trajectory_setpoint_publisher.publish(msg)
         self.get_logger().info(f"Publishing global position setpoints {position}")
 
-    def publish_vehicle_command(self, command, **params) -> None:
-        """Publish a vehicle command."""
-        msg = VehicleCommand()
-        msg.command = command
-        msg.param1 = params.get("param1", 0.0)
-        msg.param2 = params.get("param2", 0.0)
-        msg.param3 = params.get("param3", 0.0)
-        msg.param4 = params.get("param4", 0.0)
-        msg.param5 = params.get("param5", 0.0)
-        msg.param6 = params.get("param6", 0.0)
-        msg.param7 = params.get("param7", 0.0)
-        msg.target_system = 1
-        msg.target_component = 1
-        msg.source_system = 1
-        msg.source_component = 1
-        msg.from_external = True
-        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
-        self.vehicle_command_publisher.publish(msg)
-
     def timer_callback(self) -> None:
-        """Callback function for the timer."""
-        self.publish_offboard_control_heartbeat_signal()
+        position = self.coordinates[self.current_checkpoint]
+        yaw = 180.
+        self.publish_position_setpoint(position,yaw)
 
-        if self.offboard_setpoint_counter == 10:
-            self.engage_offboard_mode()
-            self.arm()
-        if self.offboard_setpoint_counter < 11:
-            self.offboard_setpoint_counter += 1
-        
-        if self.offboard_setpoint_counter > 10:
-            position = self.coordinates[self.current_checkpoint]
-            yaw = 180.
-            self.publish_position_setpoint(position,yaw)
-        
-            if np.linalg.norm(np.array(self.trans_pos(self.vehicle_odometry.position)) - np.array(self.coordinates[self.current_checkpoint])) < 0.2:
-                self.current_checkpoint += 1
-                if self.current_checkpoint == len(self.coordinates):
-                    self.land()
-                    self.disarm()
-                    self.get_logger().info("Landing")
-                    self.timer.cancel()
-                    self.destroy_node()
-                    rclpy.shutdown()
-                    return
+    def update_coordinates(self) -> None:
+        self.current_checkpoint += 0
 
-    @staticmethod
-    def trans_pos(position: list):
-        new_position = [position[1], position[0], -position[2]]
-        return new_position
-    @staticmethod
-    def trans_yaw(yaw: float):
-        new_yaw = -yaw
-        return new_yaw
-        
 def main(args=None) -> None:
     print('Starting offboard control node...')
     rclpy.init(args=args)
