5fa3f44a1516313181590bbf6a9cada2ae61421d
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-03-13 14:42:32 +0100

    merge

--- pegasus_simulation/pegasus_sim.py
+++ pegasus_simulation/pegasus_sim.py
@@ -8,9 +8,6 @@ simulation_app = SimulationApp({"headless": False})
 from pxr import Gf, UsdLux, Sdf
 
 import omni.timeline
-import omni.graph.core as og
-import omni.replicator.core as rep
-import omni.syntheticdata._syntheticdata as sd
 import omni.isaac.core.utils.numpy.rotations as rot_utils
 import omni.isaac.core.utils.prims as prims_utils
 from omni.isaac.core import World
@@ -178,44 +175,6 @@ class PegasusApp:
             ),
         )
         return camera
-        from omni.isaac.ros2_bridge import read_camera_info
-        # The following code will link the camera's render product and publish the data to the specified topic name.
-        render_product = camera._render_product_path
-        step_size = int(60/freq)
-        if vehicle_id == 0:
-            topic_name = self.topic_prefix +"/" + camera.name+"_camera_info"
-        else:
-            topic_name = self.topic_prefix +"/" + camera.name+f"_camera_info_{vehicle_id}"
-        queue_size = 1
-        node_namespace = ""
-        frame_id = camera.prim_path
-
-        writer = rep.writers.get("ROS2PublishCameraInfo")
-        camera_info = read_camera_info(render_product_path=render_product)
-        writer.initialize(
-            frameId=frame_id,
-            nodeNamespace=node_namespace,
-            queueSize=queue_size,
-            topicName=topic_name,
-            width=camera_info["width"],
-            height=camera_info["height"],
-            projectionType=camera_info["projectionType"],
-            k=camera_info["k"].reshape([1, 9]),
-            r=camera_info["r"].reshape([1, 9]),
-            p=camera_info["p"].reshape([1, 12]),
-            physicalDistortionModel=camera_info["physicalDistortionModel"],
-            physicalDistortionCoefficients=camera_info["physicalDistortionCoefficients"],
-        )
-        writer.attach([render_product])
-
-        gate_path = omni.syntheticdata.SyntheticData._get_node_path(
-            "PostProcessDispatch" + "IsaacSimulationGate", render_product
-        )
-
-        # Set step input of the Isaac Simulation Gate nodes upstream of ROS publishers to control their execution rate
-        og.Controller.attribute(gate_path + ".inputs:step").set(step_size)
-
-        return
 
     @staticmethod
     def _initialize_lidar(body_prim):
@@ -282,8 +241,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-'''
-'/quadrotor/quadrotor/body'
-'''
